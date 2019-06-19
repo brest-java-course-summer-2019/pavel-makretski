33e676c4fe3c98071226f66b7ea54a3d8ca967cb
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-19 20:21:36 +0300

    Do  myself input method

--- delivery_cost/src/com/epam/brest2019/courses/First.java
+++ delivery_cost/src/com/epam/brest2019/courses/First.java
@@ -1,8 +1,26 @@
 package com.epam.brest2019.courses;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hi!");
-        int i = 12;
-        System.out.println(i);
+
+        First first = new First();
+        BigDecimal weight = new BigDecimal(first.setVariable());
+        BigDecimal distance = new BigDecimal("10000");
+        BigDecimal pricePerKg = new BigDecimal("30");
+        BigDecimal pricePerKm = new BigDecimal("50");
+
+        BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
+        System.out.println("Total price: " + price);
+
+    }
+
+    public String setVariable(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите величину; ");
+        String var = scan.nextLine();
+        return var;
     }
 }
