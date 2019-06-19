48b57308c2d0691876c78a992cf495ae96fe0ef5
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-19 19:24:27 +0300

    Trying push witout password

--- delivery_cost/src/com/epam/brest2019/courses/First.java
+++ delivery_cost/src/com/epam/brest2019/courses/First.java
@@ -1,5 +1,8 @@
+package com.epam.brest2019.courses;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hi!");
+        int i = 12;
+        System.out.println(i);
     }
 }
