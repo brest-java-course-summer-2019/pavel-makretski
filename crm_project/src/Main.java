3ad9c60f27f79b34dd2ec0c550b49db8bba0a3ad
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-20 10:55:14 +0300

    Change project structure

--- crm_project/src/Main.java
+++ crm_project/src/Main.java
@@ -1,27 +1,6 @@
-package com.epam.brest2019.courses;
-
-import java.math.BigDecimal;
-import java.util.Scanner;
-
-public class First {
+public class Main {
     public static void main(String[] args) {
-
-        First first = new First();
-        System.out.print("Введите вес в килограммах: ");
-        BigDecimal weight = new BigDecimal(first.setVariable());
-        System.out.print("Введите расстояние в километрах: ");
-        BigDecimal distance = new BigDecimal(first.setVariable());
-        BigDecimal pricePerKg = new BigDecimal("30");
-        BigDecimal pricePerKm = new BigDecimal("50");
-
-        BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
-        System.out.println("Total price: " + price);
-
+        System.out.println("Start CRM system");
     }
 
-    public String setVariable(){
-        Scanner scan = new Scanner(System.in);
-        String var = scan.nextLine();
-        return var;
-    }
 }
