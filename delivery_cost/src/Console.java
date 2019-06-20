74f9f805427ab77b918a408878fca47b8494b907
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-20 13:36:26 +0300

    Not_null verification

--- delivery_cost/src/Console.java
+++ delivery_cost/src/Console.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Console {
+    public String setVariable() {
+        Scanner scan = new Scanner(System.in);
+        String answer, variable = null;
+        int counter = 1;
+
+        do {
+            answer = scan.nextLine();
+
+            if (answer.isEmpty()) {
+                System.out.print("ОШИБКА! Вы ничего не ввели, укажите пожалуйста правильное значение: ");
+            } else {
+                variable = answer;
+                counter = 0;
+            }
+        } while (counter == 1);
+
+
+//        if (answer.equals("q") || answer.equals("Q")) {
+//            System.out.println("Я понял что ты заблудился");
+//            // прервать выполнение программы?
+//        }
+//        // else if метод проверки на неправильные символы. Введите еще раз (поместить в цикл)
+//        // else присваиваем переменной введенное число
+
+        return variable;
+    }
 }
+
