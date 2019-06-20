a135f6324ffc628f6f814b4da55b609627913ac3
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-20 12:07:11 +0300

    Minor changes

--- delivery_cost/src/First.java
+++ delivery_cost/src/First.java
@@ -9,7 +9,9 @@ public class First {
         BigDecimal weight = new BigDecimal(first.setVariable());
         System.out.print("Введите расстояние в километрах: ");
         BigDecimal distance = new BigDecimal(first.setVariable());
+        System.out.print("Стоимость за кг будет взята из справочника");
         BigDecimal pricePerKg = new BigDecimal("30");
+        System.out.print("Стоимость за км будет взята из справочника");
         BigDecimal pricePerKm = new BigDecimal("50");
 
         BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
