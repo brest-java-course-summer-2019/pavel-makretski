daf2b857a8639c2da53b19490590d2048213eebf
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-06-20 15:16:42 +0300

    Whanna outk check creation

--- delivery_cost/src/Console.java
+++ delivery_cost/src/Console.java
@@ -9,21 +9,31 @@ public class Console {
         do {
             answer = scan.nextLine();
 
+            // is empty form check
             if (answer.isEmpty()) {
                 System.out.print("ОШИБКА! Вы ничего не ввели, укажите пожалуйста правильное значение: ");
-            } else {
+            }
+
+            // whanna out check
+            else if (answer.equals("q") || answer.equals("Q")) {
+                System.out.print("Вы действительно хотите выйти?  Y/N  ");
+                answer = scan.nextLine();
+                if (answer.equals("Y") || answer.equals("y")) {
+                    System.out.println("До свидания!");
+                    counter = 0;
+                } else System.out.print("Повторите ввод параметра: ");
+            }
+
+            // wrong simbol contains check
+
+
+            else {
                 variable = answer;
                 counter = 0;
             }
         } while (counter == 1);
 
 
-//        if (answer.equals("q") || answer.equals("Q")) {
-//            System.out.println("Я понял что ты заблудился");
-//            // прервать выполнение программы?
-//        }
-//        // else if метод проверки на неправильные символы. Введите еще раз (поместить в цикл)
-//        // else присваиваем переменной введенное число
 
         return variable;
     }
--- delivery_cost/src/First.java
+++ delivery_cost/src/First.java
@@ -5,10 +5,11 @@ public class First {
     public static void main(String[] args) {
 
         Console console = new Console();
+        System.out.println("Для расчета стоимости посылки правильно заполните данные. \nДля отмены и выхода из программы нажмите 'Q'");
 
-        System.out.print("Введите вес в килограммах: ");
+        System.out.print("Укажите вес в килограммах: ");
         BigDecimal weight = new BigDecimal(console.setVariable());
-        System.out.print("Введите расстояние в километрах: ");
+        System.out.print("Укажите расстояние в километрах: ");
         BigDecimal distance = new BigDecimal(console.setVariable());
         System.out.println("Стоимость за кг будет взята из справочника");
         BigDecimal pricePerKg = new BigDecimal("30");
