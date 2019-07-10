38985bf14a1346db62b7f0342a60a16fd1ae4010
Pavel Makretski <46418102+Polmak@users.noreply.github.com>
2019-07-10 20:02:06 +0300

    Add files via download directly. Can't do it from terminal

---
+++ try_maven/example/src/main/java/polmak/App.java
@@ -0,0 +1,12 @@
+package polmak;
+
+//Don"t
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hi World! Hope you can see this");
+    }
+}
