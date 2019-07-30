72e2f9cbba88a9faab18ae7966a474c084356e33
Pavel McRetski <46418102+Polmak@users.noreply.github.com>
2019-07-30 14:03:13 +0300

    Find bug in test-db

--- crm-project/test-db/src/main/resources/create-tables.sql
+++ crm-project/test-db/src/main/resources/create-tables.sql
@@ -1,21 +1,21 @@
 DROP TABLE IF EXISTS stage;
 CREATE TABLE stage
 (
-    sales_stage_id   INT          NOT NULL AUTO_INCREMENT,
-    sales_stage_name VARCHAR(255) NOT NULL UNIQUE,
+    sales_stage_id   INT          NOT NULL,
+    sales_stage_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (sales_stage_id)
 );
 
 DROP TABLE IF EXISTS deal;
 CREATE TABLE deal
 (
-    deal_id           INT          NOT NULL UNIQUE AUTO_INCREMENT,
+    deal_id           INT          NOT NULL,
     deal_date         DATE         NOT NULL,
     client_first_name VARCHAR(255) NOT NULL,
     client_last_name  VARCHAR(255) NOT NULL,
     budget            INT          NOT NULL,
     sales_stage_id    int          NOT NULL,
     PRIMARY KEY (deal_id),
-    FOREIGN KEY (sales_stage_id) REFERENCES stage (sales_stage_id)
+    FOREIGN KEY (sales_stage_id) REFERENCES stage(sales_stage_id)
 );
 
