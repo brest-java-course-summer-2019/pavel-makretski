6b448384a52318f6be56a427a3ec88daff72e9d8
Pavel Makretski <polmak.retski@gmail.com>
2019-08-13 20:51:40 +0300

    Forgot about test-db field

--- crm-project/test-db/src/main/resources/create-tables.sql
+++ crm-project/test-db/src/main/resources/create-tables.sql
@@ -1,20 +1,22 @@
 DROP TABLE IF EXISTS stage;
 CREATE TABLE stage
 (
-    sales_stage_id   INT          NOT NULL,
-    sales_stage_name VARCHAR(255) NOT NULL,
+    sales_stage_id      INT          NOT NULL,
+    sales_stage_name    VARCHAR(255) NOT NULL,
+    sales_stage_deals   INT          NOT NULL,
+    sales_stage_budget  INT          NOT NULL,
     PRIMARY KEY (sales_stage_id)
 );
 
 DROP TABLE IF EXISTS deal;
 CREATE TABLE deal
 (
-    deal_id           INT          NOT NULL,
-    deal_date         DATE         NOT NULL,
-    client_first_name VARCHAR(255) NOT NULL,
-    client_last_name  VARCHAR(255) NOT NULL,
-    budget            INT          NOT NULL,
-    sales_stage_id    int          NOT NULL,
+    deal_id             INT          NOT NULL,
+    deal_date           DATE         NOT NULL,
+    client_first_name   VARCHAR(255) NOT NULL,
+    client_last_name    VARCHAR(255) NOT NULL,
+    deal_budget         INT          NOT NULL,
+    sales_stage_id      INT          NOT NULL,
     PRIMARY KEY (deal_id),
     FOREIGN KEY (sales_stage_id) REFERENCES stage(sales_stage_id)
 );
